c2ffc6e4cc160e73879a40c765d6ba772f23c273
Huijing Fu <31911005@stu.zucc.edu.cn>
2026-05-25 15:58:07 +0800

    css修复

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,10 @@ const config: Config = {
   theme: {
     extend: {},
   },
-<<<<<<< HEAD
   // 修改这里：引入 typography 插件
   plugins: [
     require('@tailwindcss/typography')
   ],
-=======
->>>>>>> dc92c638413c166d9dfac55f320454291edad8fd
 };
 
 export default config;
